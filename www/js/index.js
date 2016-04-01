baff1bd94017608a5ea0483265ec7a0f8b839214
Stijn van Gaal <stijn_50@msn.com>
2016-04-01 18:30:04 +0200

    Loading in pokeman and images --too slow. need images to load async

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -47,3 +49,71 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+var api = "http://pokeapi.co/api/v2/";
+var pokemonListLimit = 20;
+var nextPokemanUri = "";
+
+var getJSON = function(url) {
+    $.get(url, function(data, success){
+        alert(data);
+        alert(success);
+        $("#PokeData").html(data);
+    });
+};
+
+var getMorePokeman = function(){
+    if(nextPokemanUri == ""){
+       nextPokemanUri = api + "pokemon/?limit="+ pokemonListLimit;
+    }
+    
+    $.get(nextPokemanUri, function(data, success){
+        nextPokemanUri = data.next;
+        data.results.forEach(function(element) {
+            thumbName = "PokemonThumb" + element.name;
+            
+            var html = [];
+            html.push(
+                "<li>",
+                "<a>",
+                "<img src='img/imgBack.png' id='"+ thumbName +"'>",
+                "<h2>",
+                element.name,
+                "</h2>",
+                "</a>",
+                "</li>"
+            );
+           
+            getPokemonSprite(element.url, thumbName);
+            
+            $("#PokeList").append(html.join(""));
+            $("#PokeList").listview("refresh");
+        }, this);
+    });
+}
+
+var getPokemonSprite = function(pokemanUrl, imgElement){
+    $.get(pokemanUrl, function(data, success){
+       $("#"+imgElement).attr("src", data.sprites.front_default); 
+    });
+}
+
+$(document).on("scrollstop", function (e) {
+    var activePage = $.mobile.pageContainer.pagecontainer("getActivePage"),
+        screenHeight = $.mobile.getScreenHeight(),
+        contentHeight = $(".ui-content", activePage).outerHeight(),
+        scrolled = $(window).scrollTop(),
+        header = $(".ui-header", activePage).hasClass("ui-header-fixed") ? $(".ui-header", activePage).outerHeight() - 1 : $(".ui-header", activePage).outerHeight(),
+        footer = $(".ui-footer", activePage).hasClass("ui-footer-fixed") ? $(".ui-footer", activePage).outerHeight() - 1 : $(".ui-footer", activePage).outerHeight(),
+        scrollEnd = contentHeight - screenHeight + header + footer;
+    $(".ui-btn-left", activePage).text("Scrolled: " + scrolled);
+    $(".ui-btn-right", activePage).text("ScrollEnd: " + scrollEnd);
+    if (activePage[0].id == "pokemanListPage" && scrolled >= scrollEnd) {
+        console.log("adding...");
+        getMorePokeman();
+    }
+});
+
+$(document).on('pageinit', function(){
+    var testpokeman = getMorePokeman();
+});
