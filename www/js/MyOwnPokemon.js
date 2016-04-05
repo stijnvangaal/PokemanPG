4ce8c18fcf7fef21290e648eb621a14252905a92
Stijn van Gaal <stijn_50@msn.com>
2016-04-05 10:48:33 +0200

    test 3

--- www/js/MyOwnPokemon.js
+++ www/js/MyOwnPokemon.js
@@ -1,12 +1,12 @@
 var captureImage = function(){
     navigator.camera.getPicture( imageCaptured, cameraError, { quality : 50,
-        destinationType: Camera.DestinationType.FILE_URI    } );
+        destinationType: Camera.DestinationType.DATA_URL    } );
 }
 
 var imageCaptured = function(imageUrl){
     window.localStorage.setItem(ownPokemonKey, imageUrl);
     $("#capturePokemonButton").html(imageUrl);
-    $("#capturePokemonImg").src = imageUrl;
+    $("#capturePokemonImg").src = "data:image/jpeg;base64," + imageUrl;
 }
 
 var cameraError = function(err){
