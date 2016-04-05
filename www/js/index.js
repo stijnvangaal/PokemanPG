b27d45a26ca079099c8af2b24bd64c78af042f58
Stijn van Gaal <stijn_50@msn.com>
2016-04-05 11:19:39 +0200

    test 7

--- www/js/index.js
+++ www/js/index.js
@@ -116,7 +116,7 @@ $("#PokeList").on('click', 'a', function(e){
         $.get(url, setDetailPage);
     }
     else{
-        $("#DetailPokemonName").html(ownImageData);
+        $("#DetailPokemonName").html("" + ownImageData);
         $("#DetailPokemonImg").attr("src", "data:image/jpeg;base64," + ownImageData);
     }
     $.mobile.changePage("#pokemanDetail");
