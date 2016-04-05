52a1d5d0482bf0bbf44c5665578c94ec3c960de2
Stijn van Gaal <stijn_50@msn.com>
2016-04-05 10:54:38 +0200

    test 4

--- www/js/MyOwnPokemon.js
+++ www/js/MyOwnPokemon.js
@@ -6,7 +6,7 @@ var captureImage = function(){
 var imageCaptured = function(imageUrl){
     window.localStorage.setItem(ownPokemonKey, imageUrl);
     $("#capturePokemonButton").html(imageUrl);
-    $("#capturePokemonImg").src = "data:image/jpeg;base64," + imageUrl;
+    $("#capturePokemonImg").attr("src", "data:image/jpeg;base64," + imageUrl);
 }
 
 var cameraError = function(err){
